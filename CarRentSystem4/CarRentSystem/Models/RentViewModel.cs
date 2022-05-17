aa2386083ed47deacfd0c71f477c385a64ee7ce6
radwaokaily <radwaokaily69@gmail.com>
2022-05-17 22:58:18 +0200

    i add fourth branch

---
+++ CarRentSystem4/CarRentSystem/Models/RentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Car_Rent_System.Models
+{
+    public class RentViewModel
+    {
+        public int Id { get; set; }
+        public string CarId { get; set; }
+        public int CustomerId { get; set; }
+        public int Fee { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public string Available { get; set; }
+    }
+}
