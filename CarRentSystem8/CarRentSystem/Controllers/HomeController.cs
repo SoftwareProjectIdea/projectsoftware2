6d913edfb03fd49cac4c4c04a5c7d31647c75c99
radwaokaily <rania.khaled0123456789za@gmail.com>
2022-05-18 08:50:00 +0200

    i add branch 8

---
+++ CarRentSystem8/CarRentSystem/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Car_Rent_System.Controllers
+{
+    public class HomeController : Controller
+    {   //TODO: Login Page For The Admin.
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarRentSystem8/CarRentSystem/Models/CarRegisterValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Car_Rent_System.Models
+{
+    [MetadataType(typeof(CarRegisterMetaData))]
+    public partial class CarRegister
+    {
+        public class CarRegisterMetaData
+        {
+            [DisplayName("Car Number")]
+            public string CarNumber { get; set; }
+            [DisplayName("Brand")]
+            public string CarBrand { get; set; }
+            [DisplayName("Model")]
+            public string CarModel { get; set; }
+            [DisplayName("Available")]
+            public string Available { get; set; }
+        }
+
+    }
+}
